5249c847b85081ff5d4bcc47778d0bcc6e16562f
Phú Phan <51303000@hcmut.edu.vn>
2017-10-22 17:08:41 +0700

    update

--- app/src/main/java/bk/lvtn/ReportDetailActivity.java
+++ app/src/main/java/bk/lvtn/ReportDetailActivity.java
@@ -1,10 +1,14 @@
 package bk.lvtn;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,20 +23,52 @@ public class ReportDetailActivity extends AppCompatActivity {
     ListView listField;
     ArrayList<Field> arrField = new ArrayList<Field>();
     FieldAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_detail);
         listField = (ListView) findViewById(R.id.list_field);
-        adapter = new FieldAdapter(this,arrField,R.layout.item_inlist_field);
+        adapter = new FieldAdapter(this, arrField, R.layout.item_inlist_field);
 
         listField.setAdapter(adapter);
         // Test listview
         Field test = new Field();
         test.setKey_field("Địa điểm");
         test.setValue_field("Báo cáo kết quả thường kỳ");
+
+        Field test1 = new Field();
+        test1.setKey_field("Địa asdasd");
+        test1.setValue_field("Báo cáo kếasdasdasdsat quả thường kỳ");
         arrField.add(test);
-        arrField.add(test);
+        arrField.add(test1);
         adapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case 100: {
+//                if (resultCode == (RESULT_OK && null != data) {
+
+//                ArrayList<String> result = data
+//                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                Toast.makeText(this, result.get(0),
+//                        Toast.LENGTH_SHORT).show();
+//                Bundle eee = data.getBundleExtra("position");
+//
+//                Field field = adapter.getItem(eee.getInt("pos"));
+////                    field = listField.get(pos);
+//                field.setValue_field("vcvcvcv");
+////                    valueField.setText("vcvcvcv");
+////                    valueField.setSelection(valueField.getText().length());
+//                adapter.notifyDataSetChanged();
+//                }
+                break;
+            }
+
+        }
     }
 }
--- app/src/main/java/bk/lvtn/fragment_adapter/FieldAdapter.java
+++ app/src/main/java/bk/lvtn/fragment_adapter/FieldAdapter.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +33,8 @@ public class FieldAdapter extends ArrayAdapter<Field> {
     ArrayList<Field> listField;
     int resId;
     EditText valueField = null;
+    Field field;
+    int pos;
     public FieldAdapter(Context context, ArrayList<Field> listField, int resId){
         super(context,resId,listField);
         this.context = context;
@@ -39,23 +43,24 @@ public class FieldAdapter extends ArrayAdapter<Field> {
     }
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = layoutInflater.inflate(resId,null);
         TextView keyField = (TextView)convertView.findViewById(R.id.key_field);
         valueField = (EditText) convertView.findViewById(R.id.value_field);
         Button voiceButton = (Button) convertView.findViewById(R.id.voice_button);
-        Field field = listField.get(position);
+        field = listField.get(position);
         keyField.setText(field.getKey_field());
         valueField.setText(field.getValue_field());
 
         voiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                promptSpeechInput();
-                valueField.setText("aaaa");
-                String aaa= valueField.getText().toString();
-                notifyDataSetChanged();
+                promptSpeechInput();
+//                field = listField.get(position);
+//                pos = position;
+//                field.setValue_field("aaaa");
+
             }
         });
         return convertView;
@@ -68,6 +73,9 @@ public class FieldAdapter extends ArrayAdapter<Field> {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
                 context.getString(R.string.speech_prompt));
+        Bundle bundle = new Bundle();
+        bundle.putInt("pos",pos);
+        intent.putExtra("position",bundle);
         try {
             ((Activity)context).startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
@@ -76,22 +84,25 @@ public class FieldAdapter extends ArrayAdapter<Field> {
                     Toast.LENGTH_SHORT).show();
         }
     }
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//
-//        switch (requestCode) {
-//            case REQ_CODE_SPEECH_INPUT: {
-//                if (resultCode == ((Activity)context).RESULT_OK && null != data) {
-//
-//                    ArrayList<String> result = data
-//                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-//                                Toast.makeText(context, result.get(0),
-//                    Toast.LENGTH_SHORT).show();
-//                    valueField.setText("vcvcvcv");
-//                    valueField.setSelection(valueField.getText().length());
-//                }
-//                break;
-//            }
-//
-//        }
-//    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == ((Activity)context).RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                                Toast.makeText(context, result.get(0),
+                    Toast.LENGTH_SHORT).show();
+                    field = listField.get(pos);
+                    field.setValue_field("vcvcvcv");
+                    valueField.setText("vcvcvcv");
+                    valueField.setSelection(valueField.getText().length());
+                    notifyDataSetChanged();
+                }
+                break;
+            }
+
+        }
+    }
 }
