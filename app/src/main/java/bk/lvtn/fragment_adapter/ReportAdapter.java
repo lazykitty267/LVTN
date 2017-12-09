eea522b9d817b1add1135f6a0a650234fca71c8a
Phú Phan <51303000@hcmut.edu.vn>
2017-12-09 15:06:46 +0700

    create Success dialog when save file successful

--- app/src/main/java/bk/lvtn/fragment_adapter/ReportAdapter.java
+++ app/src/main/java/bk/lvtn/fragment_adapter/ReportAdapter.java
@@ -3,6 +3,15 @@ package bk.lvtn.fragment_adapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,6 +21,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -45,7 +57,24 @@ public class ReportAdapter extends ArrayAdapter<Report> {
         convertView = layoutInflater.inflate(resId,null);
         TextView reportName = (TextView)convertView.findViewById(R.id.report_name);
         TextView reportCreateDate = (TextView) convertView.findViewById(R.id.report_create_date);
-        FloatingActionButton mod_button = (FloatingActionButton) convertView.findViewById(R.id.modify_report_button);
+//        LinearLayout linearLayout = (LinearLayout) convertView.findViewById(R.id.wrap_layout);
+//        if((position%2)==1){
+//            linearLayout.setBackgroundResource(R.drawable.round2);
+//        }
+//        else {
+//            linearLayout.setBackgroundResource(R.drawable.round);
+//        }
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.img_rp_thumbnai) ;
+
+        Bitmap mbitmap = ((BitmapDrawable) context.getResources().getDrawable(R.drawable.report_thumbnai)).getBitmap();
+        Bitmap imageRounded = Bitmap.createBitmap(mbitmap.getWidth(), mbitmap.getHeight(), mbitmap.getConfig());
+        Canvas canvas = new Canvas(imageRounded);
+        Paint mpaint = new Paint();
+        mpaint.setAntiAlias(true);
+        mpaint.setShader(new BitmapShader(mbitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+        canvas.drawRoundRect((new RectF(0, 0, mbitmap.getWidth(), mbitmap.getHeight())), 100, 100, mpaint);// Round Image Corner 100 100 100 100
+        imageView.setImageBitmap(imageRounded);
+        ImageButton mod_button = (ImageButton) convertView.findViewById(R.id.modify_report_button);
         final Report report = listReport.get(position);
         reportName.setText(report.getReportName());
         reportCreateDate.setText(report.getCreateDate());
