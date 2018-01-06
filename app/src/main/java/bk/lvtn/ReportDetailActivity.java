6860fb21f852dfec4627112eb5345c4c34751035
lazykitty <lazykitty267@gmail.com>
2018-01-06 15:20:26 +0700

    Add

--- app/src/main/java/bk/lvtn/ReportDetailActivity.java
+++ app/src/main/java/bk/lvtn/ReportDetailActivity.java
@@ -555,13 +555,14 @@ public class ReportDetailActivity extends AppCompatActivity {
                                             //You'll need to add proper error handling here
                                         }
                                         pdfFile.setPublicKeyUrl(text.toString().replace("\u0000", ""));
-                                        while (pdfFile.getSignUrl() != null
-                                                && pdfFile.getPublicKeyUrl() != null
-                                                && pdfFile.getUrl() != null) {
-                                            dataService.uploadFile(file, pdfFile);
-                                            Toast.makeText(ReportDetailActivity.this,"ONLINE MODE",Toast.LENGTH_SHORT);
-                                            showSuccessDialog();
+                                        while (pdfFile.getSignUrl() == null
+                                                || pdfFile.getPublicKeyUrl() == null
+                                                || pdfFile.getUrl() == null) {
+
                                         }
+                                        dataService.uploadFile(file, pdfFile);
+                                        Toast.makeText(ReportDetailActivity.this,"ONLINE MODE",Toast.LENGTH_SHORT);
+                                        showSuccessDialog();
                                     }
                                 } catch (Exception e) {
                                     Toast.makeText(ReportDetailActivity.this,e.toString(),Toast.LENGTH_SHORT);
